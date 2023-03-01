61888ce37d3ba8b612f8bd3db8c3a1eb8cc4a399
Anton Charny <anton.charny@perimeter81.com>
2023-03-01 12:15:10 +0200

    Initial commit + done

---
+++ Marathon_1/GradientView.swift
@@ -0,0 +1,51 @@
+//
+//  GradientView.swift
+//  Marathon_1
+//
+//  Created by Anton Charny on 01/03/2023.
+//
+
+import UIKit
+
+class GradientView: UIView {
+    
+    public var gradientColors: [UIColor] = []
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        self.setGradient(colors: self.gradientColors)
+    }
+    
+    //MARK: - Private
+    private func setGradient(colors: [UIColor]) {
+        guard !colors.isEmpty, let context = UIGraphicsGetCurrentContext() else { return }
+        let locations = self.setColoursLocations(colorsCount: colors.count)
+        
+        guard let gradient = CGGradient(colorsSpace: CGColorSpaceCreateDeviceRGB(),
+                                        colors: colors.map { $0.cgColor } as CFArray,
+                                        locations: locations) else { return }
+        
+        let path = UIBezierPath(roundedRect: self.bounds,
+                                cornerRadius: self.layer.cornerRadius)
+        context.saveGState()
+        path.addClip()
+        
+        let startPoint = colors.count == 2 ? CGPoint(x: -bounds.width / 3, y: -bounds.height / 3) : .zero
+        let endPoint = CGPoint(x: bounds.width, y: bounds.height)
+        
+        context.drawLinearGradient(gradient, start: startPoint, end: endPoint, options: .drawsBeforeStartLocation)
+        context.restoreGState()
+    }
+    
+    private func setColoursLocations(colorsCount: Int) -> [CGFloat] {
+        var locations = [CGFloat]()
+        for colorIndex in 0..<colorsCount {
+            if colorIndex == colorsCount - 1 {
+                locations.append(1)
+                break
+            }
+            locations.append(CGFloat(colorIndex) * 0.25)
+        }
+        return locations
+    }
+}
--- Marathon_1/ViewController.swift
+++ Marathon_1/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var squareView: GradientView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.setupView()
+        let colours = [UIColor.blue,
+                       UIColor.red]
+        self.squareView.gradientColors = colours
+        self.addShadow()
+
     }
-
-
+    
+    private func setupView() {
+        self.squareView.layer.cornerRadius = 16
+    }
+    
+    /*
+     private func addGradientLayer() { //такой способ неинтересный
+     let gradientLayer = CAGradientLayer()
+     gradientLayer.colors = colors
+     gradientLayer.startPoint = startPoint
+     gradientLayer.endPoint = endPoint
+     gradientLayer.frame = self.squareView.bounds
+     gradientLayer.cornerRadius = self.squareView.layer.cornerRadius
+     
+     self.squareView.layer.insertSublayer(gradientLayer, at: 0)
+     }
+     */
+    
+    private func addShadow() {
+        let path = UIBezierPath(roundedRect: self.squareView.bounds,
+                                cornerRadius: self.squareView.layer.cornerRadius)
+        
+        self.squareView.layer.shadowPath = path.cgPath
+        self.squareView.layer.shadowOffset = CGSize(width: 5, height: 10)
+        self.squareView.layer.shadowOpacity = 1
+        self.squareView.layer.shadowRadius = 3
+        self.squareView.layer.shadowColor = UIColor.gray.cgColor
+        self.squareView.layer.masksToBounds = false
+    }
+    
 }
 
